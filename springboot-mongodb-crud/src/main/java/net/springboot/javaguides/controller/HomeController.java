99b22199fee787b1374fe82b4f4870870a2f40ce
karlorz <karl.logon@gmail.com>
2024-02-10 20:27:28 +0800

    update

--- springboot-mongodb-crud/src/main/java/net/springboot/javaguides/controller/HomeController.java
+++ springboot-mongodb-crud/src/main/java/net/springboot/javaguides/controller/HomeController.java
@@ -1,2 +1,10 @@
-package net.springboot.javaguides.controller;public class HomeController {
+package net.springboot.javaguides.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class HomeController {
+    @GetMapping("/")
+    public String index() {
+        return "static/index.html";
+    }
 }
